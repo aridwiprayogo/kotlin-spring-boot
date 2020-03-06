c20bc72e7a183fe144ebc300ec00b5f28c69d724
aridwiprayogo <kisamberbledek@yahoo.com>
2020-03-06 17:37:04 +0700

    update id from auto increment to UUID

--- src/main/kotlin/com/aridwiprayogo/kotlinspringboot/NewsRepository.kt
+++ src/main/kotlin/com/aridwiprayogo/kotlinspringboot/NewsRepository.kt
@@ -2,6 +2,7 @@ package com.aridwiprayogo.kotlinspringboot
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface NewsRepository : JpaRepository<News, Long>
+interface NewsRepository : JpaRepository<News, UUID>
